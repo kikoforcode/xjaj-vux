dfd42d732e56fe5b923b6c95dc3744cccef4379a
Yvette <zhangqianqian808@163.com>
2018-11-07 20:15:01 +0800

    first commit again

---
+++ src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import FastClick from 'fastclick'
+import VueRouter from 'vue-router'
+import App from './App'
+import Home from './components/HelloFromVux'
+// 全局引入toast、alert、loading
+import { ToastPlugin, AlertPlugin, LoadingPlugin } from 'vux'
+
+Vue.use(VueRouter)
+Vue.use(ToastPlugin)
+Vue.use(AlertPlugin)
+Vue.use(LoadingPlugin)
+
+const routes = [{
+  path: '/',
+  component: Home
+}]
+
+const router = new VueRouter({
+  routes
+})
+
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app-box')
